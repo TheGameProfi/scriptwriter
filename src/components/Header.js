53a9193ea9cb15e7c5027137aaba5dcc5fa93356
Marc <mbg14.gaming@gmail.com>
2024-07-05 17:13:08 +0200

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from '@/styles/Header.module.css';
+import Image from 'next/image';
+import { Button } from "@mui/material";
+import { Home, Leaderboard } from "@mui/icons-material";
+import Link from 'next/link';
 
 export default function Header() {
     return (
-        <header>
-            <h1>Header 2</h1>
-        </header>
+        <header className={styles.head}>
+            <div className={styles.title}>
+                <h1>MonkeyScript</h1>
+            </div>
+            <div className={styles.nav}>
+                <Link href="/">
+                    <Button variant="text" color="primary" startIcon={<Home />}>Home</Button>
+                </Link>
+                <Link href="/leaderboard">
+                    <Button variant="text" color="primary" startIcon={<Leaderboard />}>Leaderboard</Button>
+                </Link>
+            </div>
+        </header >
     );
 }
